365ff281a6adcb98aa872e93e784d949a39d21d1
MarijaPaun <95476002+MarijaPaun@users.noreply.github.com>
2021-12-22 14:39:42 +0100

    Add files via upload

---
+++ exercise2.js
@@ -0,0 +1,21 @@
+console.log("Conected")
+
+console.log("phonePrice")
+let phonePrice = 119.95
+console.log(phonePrice)
+
+console.log("phoneTax")
+let phoneTax = phonePrice * 0.05
+console.log(phoneTax)
+
+console.log("phonePriceWithTax")
+let phonePriceWithTax = phonePrice + phoneTax
+console.log(phonePriceWithTax)
+
+console.log("pnoheAmount")
+let pnoheAmount = 30
+console.log(pnoheAmount)
+
+console.log("totalPurchase")
+let totalPurchase = phonePriceWithTax * pnoheAmount
+console.log(totalPurchase)
